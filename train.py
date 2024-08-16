306fdb54e382185ad056516a62869853fed10519
riverstone496 <robosatoki@outlook.jp>
2024-08-16 20:11:44 +0900

    add new version psgd

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import argparse
 import torch
 import torch.nn.functional as F
 from torchvision import datasets, transforms
-import preconditioned_stochastic_gradient_descent as psgd
+from preconditioned_stochastic_gradient_descent import Affine
 import wandb
 
 def train_loss(data, target, model):
@@ -28,10 +28,13 @@ if __name__ == "__main__":
     parser.add_argument('--test_batch_size', type=int, default=1000, help='input batch size for testing')
     parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train')
     parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
-    parser.add_argument('--curvature_lr', type=float, default=0.01, help='learning rate')
-    parser.add_argument('--model', type=str, default='mlp', help='number of epochs to train')
+    parser.add_argument('--preconditioner_lr', type=float, default=0.01, help='learning rate')
+    parser.add_argument('--momentum', type=float, default=0.2, help='learning rate')
+    parser.add_argument('--model', type=str, default='mlp_tanh', help='number of epochs to train')
     parser.add_argument('--optim', type=str, default='psgd', help='number of epochs to train')
     parser.add_argument('--width', type=int, default=1024, help='number of epochs to train')
+    parser.add_argument('--weight_decay', type=float, default=0.1, help='learning rate')
+    parser.add_argument('--preconditioner_init_scale', type=float, default=1.0, help='learning rate')
     args = parser.parse_args()
     wandb.init(config=args)
     
@@ -42,44 +45,52 @@ if __name__ == "__main__":
         datasets.MNIST('../data', train=False, transform=transforms.Compose([transforms.ToTensor()])),    
         batch_size=args.test_batch_size, shuffle=False)
 
+    if torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cuda'
     if args.model == 'lenet5':
         model = LeNet5()
-    if args.model == 'mlp':
+    elif args.model == 'mlp_relu':
         model = MLP(n_hid = args.width)
+    elif args.model == 'mlp_tanh':
+        model = MLP(n_hid = args.width, nonlin=torch.tanh)
 
-    Qs = [[torch.eye(W.shape[0]), torch.eye(W.shape[1])] for W in model.parameters()]
-    grad_norm_clip_thr = 0.1 * sum(W.numel() for W in model.parameters()) ** 0.5
-    TrainLosses, best_test_loss = [], 1.0
+    opt = Affine(
+        model.parameters(),
+        # rank_of_approximation=rank,
+        preconditioner_init_scale=args.preconditioner_init_scale,
+        lr_params=args.lr,
+        lr_preconditioner=args.preconditioner_lr,
+        step_normalizer="1st", #this does Approx Newton on Lie Group
+        momentum=args.momentum,   # match the momentum with Adam so that only their 'preconditioners' are different
+        grad_clip_max_norm=10.0,
+    )
 
-    for epoch in range(args.epochs):
-        for batch_idx, (data, target) in enumerate(train_loader):
-            loss = train_loss(data, target, model) + 1e-6 * sum([torch.sum(p * p) for p in model.parameters()])
-            grads = torch.autograd.grad(loss, model.parameters(), create_graph=True)
-            if args.optim == 'psgd':
-                vs = [torch.randn_like(W) for W in model.parameters()]
-                Hvs = torch.autograd.grad(grads, model.parameters(), vs)
-                with torch.no_grad():
-                    Qs = [psgd.update_precond_kron(Qlr[0], Qlr[1], v, Hv, step=args.curvature_step) for (Qlr, v, Hv) in zip(Qs, vs, Hvs)]
-                    pre_grads = [psgd.precond_grad_kron(Qlr[0], Qlr[1], g) for (Qlr, g) in zip(Qs, grads)]
-                    grad_norm = torch.sqrt(sum([torch.sum(g * g) for g in pre_grads]))
-                    lr_adjust = min(grad_norm_clip_thr / grad_norm, 1.0)
-                    [W.subtract_(lr_adjust * args.lr * g) for (W, g) in zip(model.parameters(), pre_grads)]
-                    TrainLosses.append(loss.item())
-                    if batch_idx % 3 == 0:
-                        dgPdg = [torch.sum((Qlr[0] @ dg @ Qlr[1].T)**2) for (Qlr, dg) in zip(Qs, Hvs)]
-                        dxPinvdx = [torch.sum((torch.linalg.inv(Qlr[0].T) @ dx @ torch.linalg.inv(Qlr[1]))**2) for (Qlr, dx) in zip(Qs, vs)]
-                        criterion = sum(dgPdg) + sum(dxPinvdx)
-                        iteration = len(train_loader) * epoch + batch_idx
-                        print(f"Epoch:{epoch+1} Iteration:{iteration}, Criteion:{criterion}")
-                        wandb.log({"Epoch": epoch + 1, "Iteration":iteration, "Criteion": criterion, "TrainLoss":loss.item()})
-            if args.optim == 'sgd':
-                with torch.no_grad():
-                    grad_norm = torch.sqrt(sum([torch.sum(g * g) for g in grads]))
-                    lr_adjust = min(grad_norm_clip_thr / grad_norm, 1.0)
-                    [W.subtract_(lr_adjust * args.lr * g) for (W, g) in zip(model.parameters(), grads)]
-    
-        test_err_rate = test_loss(test_loader, model)
-        best_test_loss = min(best_test_loss, test_err_rate)
-        args.lr *= (0.01) ** (1 / (args.epochs - 1))
-        wandb.log({"Epoch": epoch + 1, "TestErrorRate": test_err_rate, "BestTestLoss":best_test_loss, "TestAcc":100*(1-test_err_rate)})
-        print(f'Epoch: {epoch + 1}; best test classification error rate: {best_test_loss}')
+    TrainLosses, best_test_loss, LogDets = [], 1.0, []
+    for epoch in range(20):
+        for _, (data, target) in enumerate(train_loader):
+            data = data.to(device)
+            target = target.to(device)
+
+            def closure():
+                xentropy = train_loss(data, target)
+                l2 = sum(
+                    [
+                        torch.sum(torch.rand_like(p) * p * p)
+                        for p in opt._params_with_grad
+                    ]
+                )
+                return xentropy + args.weight_decay * l2, xentropy
+
+            _, loss = opt.step(closure)
+            TrainLosses.append(loss.item())
+
+        best_test_loss = min(best_test_loss, test_loss())
+        opt.lr_params *= (0.01) ** (1 / 19)
+        opt.lr_preconditioner *= (0.01) ** (1 / 19)
+        print(
+            "Epoch: {}; best test classification error rate: {}".format(
+                epoch + 1, best_test_loss
+            )
+        )
